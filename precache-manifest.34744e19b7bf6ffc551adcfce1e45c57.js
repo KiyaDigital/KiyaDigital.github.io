a9d89c2d583eaae36d79fc0eb5a1e94d43b678b8
Kiya Thurman <kiya@unitedpixelpushers.com>
2019-06-26 18:57:44 -0500

    Initial commit

---
+++ precache-manifest.34744e19b7bf6ffc551adcfce1e45c57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2293f501ea30a2c4a33f1c6171869fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c08a1d8cb2b24d07fd3",
+    "url": "/static/css/main.58f05747.chunk.css"
+  },
+  {
+    "revision": "66e3a1ad88d0a99012d0",
+    "url": "/static/js/2.d428dbfa.chunk.js"
+  },
+  {
+    "revision": "7c08a1d8cb2b24d07fd3",
+    "url": "/static/js/main.e2f79f91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
